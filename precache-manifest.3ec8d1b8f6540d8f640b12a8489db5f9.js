9c469f4cddcf7d544cf20b279438f24b9358e853
Van-Code <vtran@vant-mac.fios-router.home>
2020-04-26 22:52:05 -0400

    Updates

---
+++ precache-manifest.3ec8d1b8f6540d8f640b12a8489db5f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c4d63e5f222734401a218b8f6f3d6fe",
+    "url": "/Van-Code/react-simple/index.html"
+  },
+  {
+    "revision": "b0ae10acaff941baf41b",
+    "url": "/Van-Code/react-simple/static/css/main.eca56c4a.chunk.css"
+  },
+  {
+    "revision": "98a2e582d71934608c6a",
+    "url": "/Van-Code/react-simple/static/js/2.d3ad641e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Van-Code/react-simple/static/js/2.d3ad641e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0ae10acaff941baf41b",
+    "url": "/Van-Code/react-simple/static/js/main.3a65201d.chunk.js"
+  },
+  {
+    "revision": "6b8fa071c1872c3fc856",
+    "url": "/Van-Code/react-simple/static/js/runtime-main.21b34466.js"
+  }
+]);
